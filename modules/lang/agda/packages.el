340c86b95ee9de45363600e9636fc3e957a4bdf2
citrusmunch <citrusmunch@users.noreply.github.com>
2020-07-20 13:45:52 -0400

    Revert "change pinned agda version"

--- modules/lang/agda/packages.el
+++ modules/lang/agda/packages.el
@@ -6,11 +6,11 @@
     :recipe (:host github :repo "agda/agda"
              :files ("src/data/emacs-mode/agda-input.el")
              :nonrecursive t)
-    :pin "8eb0d01811a663cf2b27b482b3b18690adfa094b")
+    :pin "ff9173e14e")
 
   (package! agda2-mode
     :recipe (:host github :repo "agda/agda"
              :files ("src/data/emacs-mode/*.el"
                      (:exclude "agda-input.el"))
              :nonrecursive t)
-    :pin "8eb0d01811a663cf2b27b482b3b18690adfa094b"))
+    :pin "ff9173e14e"))
